c0cac01543a88e1f8ccaa64719e58ddbac0a6180
JULIO ANTHONY ENGELS RUIZ COTO <jaruizc@correo.url.edu.gt>
2021-09-22 20:38:11 -0600

    Agregar archivos de proyecto.

---
+++ Node.cpp
@@ -0,0 +1,8 @@
+#include "Node.h"
+
+Node::Node() {
+	next = nullptr; // el ultimo apuntara como null
+}
+Node::~Node() {
+	next = nullptr;
+}
---
+++ Node.h
@@ -0,0 +1,11 @@
+#pragma once
+class Node
+{
+public:
+	int value; // variable 
+	Node* next;//apuntador que apunte a siguiente
+	//constructor y destructor
+	Node();
+	~Node();
+};
+
